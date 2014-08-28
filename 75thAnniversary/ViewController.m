8969187054ea46aa6797de36d51df2d16f05b1d8
Joshua <jrscorca@gmail.com>
2014-08-28 01:02:43 -0700

    +thumbnail cropping

--- 75thAnniversary/ViewController.m
+++ 75thAnniversary/ViewController.m
@@ -28,20 +28,25 @@
 }
 
 -(void)setupGrid{
-    for (int i = 0; i<20; ++i) {
+    for (int i = 0; i<21; ++i) {
         
-        UIImage *image = [UIImage imageNamed:@"google"];
+        UIImage *IMG = [UIImage imageNamed: [NSString stringWithFormat:@"full%i.jpg", (i+1)]];
+        UIImage *image = [self imageWithImage:IMG scaledToFillSize:CGSizeMake(128, 128)];
+        if (!image) continue;
+        
         NSLog(@"%f, %f", image.size.width, image.size.height);
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
         button.backgroundColor = [UIColor blackColor];
         [button setImage:image forState:UIControlStateNormal];
         [button addTarget:self action:@selector(thumbnailTouched:) forControlEvents:UIControlEventTouchUpInside];
         button.tag = i+1;
+        //button.imageView.contentMode = UIViewContentModeScaleAspectFit;
+        button.imageView.contentMode = UIViewContentModeRedraw;
         
         
-        [button setFrame:CGRectMake(20+((image.size.width+6)*(i%4)), 10+((image.size.height+6)*(i/4)), image.size.width, image.size.height)];
+        [button setFrame:CGRectMake(20+((128+6)*(i%4)), 10+((128+6)*(floor(i/4))), 128, 128)];
         [self.scrollview addSubview:button];
-        [self.scrollview setContentSize:CGSizeMake(320, 10+(image.size.height+6)*((i/4)+1))];
+        [self.scrollview setContentSize:CGSizeMake(320, 10+(128+6)*((floor(i/4))+1))];
     }
     
     
@@ -56,7 +61,7 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if([segue.identifier isEqualToString:@"transition"]) {
         DetailViewController *DVC = (DetailViewController*)segue.destinationViewController;
-        DVC.index = ((UIButton*)sender).tag;
+        DVC.index = ((UIButton*)sender).tag-1;
     }
 }
 
@@ -66,6 +71,23 @@
 
 -(IBAction)shareButtonTouched:(id)sender{
     NSLog(@"share button pressed");
+}
+
+-(UIImage *)imageWithImage:(UIImage *)image scaledToFillSize:(CGSize)size
+{
+    CGFloat scale = MAX(size.width/image.size.width, size.height/image.size.height);
+    CGFloat width = image.size.width * scale;
+    CGFloat height = image.size.height * scale;
+    CGRect imageRect = CGRectMake((size.width - width)/2.0f,
+                                  (size.height - height)/2.0f,
+                                  width,
+                                  height);
+    
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    [image drawInRect:imageRect];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 @end
