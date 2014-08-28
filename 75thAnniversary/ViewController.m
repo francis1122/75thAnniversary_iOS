6e15b00e3295ff550fba16a40a8834015fbc61c3
Joshua <jrscorca@gmail.com>
2014-08-28 01:28:38 -0700

    +changed thumbnail format

--- 75thAnniversary/ViewController.m
+++ 75thAnniversary/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 #import "DetailViewController.h"
 
+#define IS_IPHONE5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+
 @interface ViewController ()
 
 @end
@@ -28,34 +30,39 @@
 }
 
 -(void)setupGrid{
-    for (int i = 0; i<21; ++i) {
-        
-        UIImage *IMG = [UIImage imageNamed: [NSString stringWithFormat:@"full%i.jpg", (i+1)]];
-        UIImage *image = [self imageWithImage:IMG scaledToFillSize:CGSizeMake(128, 128)];
-        if (!image) continue;
-        
-        NSLog(@"%f, %f", image.size.width, image.size.height);
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        button.backgroundColor = [UIColor blackColor];
-        [button setImage:image forState:UIControlStateNormal];
-        [button addTarget:self action:@selector(thumbnailTouched:) forControlEvents:UIControlEventTouchUpInside];
-        button.tag = i+1;
-        //button.imageView.contentMode = UIViewContentModeScaleAspectFit;
-        button.imageView.contentMode = UIViewContentModeRedraw;
-        
-        
-        [button setFrame:CGRectMake(20+((128+6)*(i%4)), 10+((128+6)*(floor(i/4))), 128, 128)];
-        [self.scrollview addSubview:button];
-        [self.scrollview setContentSize:CGSizeMake(320, 10+(128+6)*((floor(i/4))+1))];
+    float squareSize = 134;
+    float xOffset = 6;
+    int numOfColumns = 4;
+    if (!IS_IPHONE5) {
+        squareSize = 152;
+        numOfColumns = 3;
     }
-    
-    
+        for (int i = 0; i<21; ++i) {
+            
+            UIImage *IMG = [UIImage imageNamed: [NSString stringWithFormat:@"full%i.jpg", (i+1)]];
+            UIImage *image = [self imageWithImage:IMG scaledToFillSize:CGSizeMake(squareSize, squareSize)];
+            if (!image) continue;
+            
+            NSLog(@"%f, %f", image.size.width, image.size.height);
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+            button.backgroundColor = [UIColor blackColor];
+            [button setImage:image forState:UIControlStateNormal];
+            [button addTarget:self action:@selector(thumbnailTouched:) forControlEvents:UIControlEventTouchUpInside];
+            button.tag = i+1;
+            //button.imageView.contentMode = UIViewContentModeScaleAspectFit;
+            button.imageView.contentMode = UIViewContentModeRedraw;
+            
+            
+            [button setFrame:CGRectMake(xOffset+((squareSize+6)*(i%numOfColumns)), 6+((squareSize+6)*(floor(i/numOfColumns))), squareSize, squareSize)];
+            [self.scrollview addSubview:button];
+            [self.scrollview setContentSize:CGSizeMake(320, 6+(squareSize+6)*((floor(i/numOfColumns))+1))];
+        }
     
 }
 
 -(void)thumbnailTouched:(id)sender{
     [self performSegueWithIdentifier:@"transition" sender:sender];
-
+    
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
